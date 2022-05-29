1d2343e54ce55e265c418fcf1a2adb0a6acfa3d2
AzherSaeed <chazher1997@gmail.com>
2022-05-29 12:30:39 +0500

    labour item set

--- src/Layout/Estimates/CreateNew/AddItems/Index.js
+++ src/Layout/Estimates/CreateNew/AddItems/Index.js
@@ -6,12 +6,41 @@ import CustomButton from "../../../../Components/CustomButton/Index";
 import { Tabs, Radio } from "antd";
 import MobileLabourLine from "./MobileLabourLine";
 import MobileMaterialLine from "./MobileMaterialLine";
+import { useMutation, useQuery } from "react-query";
+import axios from "axios";
+import { API_URL, LINE_ITEMS_GET } from "../../../../Services/config";
+
 
 const { TabPane } = Tabs;
 const Index = () => {
   const [tab, setTab] = useState(1);
+
+
+  const { data : {data : lineData}, isLoading, isSuccess, error, isError , refetch } = useQuery(
+    "getLineItem",
+    () => {
+      return axios.get(API_URL + LINE_ITEMS_GET, {
+        headers: {
+          "Content-Type": "application/json",
+          "Access-Control-Allow-Origin": "*",
+        },
+      });
+    },
+    {
+      onSuccess: (data) => {
+        console.log(data);
+      },
+      refetchInterval: false,
+      refetchOnWindowFocus: false,
+    }
+  );
+
+
+  console.log(lineData , 'data');
+
+
+
   function callback(key) {
-    // console.log(key,'tab');
     setTab(key);
   }
   return (
@@ -20,46 +49,43 @@ const Index = () => {
         <div className="main-container">
           <div className="row">
             <div className="col-md-6 col-sm-12">
-
-
               <div className="first-table">
                 <Tabs defaultActiveKey="1" onChange={callback}>
                   <TabPane tab="Labor Lineitems" key="1">
-                    {Array(10).fill('').map((_, index) =>
-                      <div className='addItem'>
-                        {/* <Link to='/estimates/createNew/addItem'> */}
+                    {Array(10)
+                      .fill("")
+                      .map((_, index) => (
+                        <div className="addItem">
+                          {/* <Link to='/estimates/createNew/addItem'> */}
 
+                          <div className="addItem-div">
+                            <div>Project Manager</div>
+                            <div>&gt;</div>
+                          </div>
 
-                        <div className='addItem-div'>
-                          <div>Project Manager</div>
-                          <div>&gt;</div>
+                          {/* </Link> */}
                         </div>
-
-                        {/* </Link> */}
-                      </div>
-                    )}
+                      ))}
                   </TabPane>
                   <TabPane tab="Materials Lineitems" key="2">
-                    {Array(10).fill('').map((_, index) =>
-                      <div className='addItem'>
-                        {/* <Link to='/estimates/createNew/addItem'> */}
-                        <div className="d-none d-sm-block">
+                    {Array(10)
+                      .fill("")
+                      .map((_, index) => (
+                        <div className="addItem">
+                          {/* <Link to='/estimates/createNew/addItem'> */}
+                          <div className="d-none d-sm-block">
+                            <div>Project Manager</div>
+                            <div>&gt;</div>
+                          </div>
 
-
-                          <div>Project Manager</div>
-                          <div>&gt;</div>
+                          {/* </Link> */}
                         </div>
-
-                        {/* </Link> */}
-                      </div>
-                    )}
+                      ))}
                   </TabPane>
                 </Tabs>
               </div>
-
             </div>
             <div className="col-md-6 col-sm-12 mt-3">
-
               <div className="second-table">
                 <div className="d-none d-sm-block">
                   <div className="main-heading">
@@ -81,8 +107,14 @@ const Index = () => {
                               addonAfter="Rate"
                               defaultValue={20}
                             />
-                            <InputNumber addonAfter="Quantity" defaultValue={20} />
-                            <InputNumber addonBefore="Total" defaultValue={20} />
+                            <InputNumber
+                              addonAfter="Quantity"
+                              defaultValue={20}
+                            />
+                            <InputNumber
+                              addonBefore="Total"
+                              defaultValue={20}
+                            />
                           </div>
                         </div>
                         <div className="rateWrapper">
@@ -93,7 +125,10 @@ const Index = () => {
                               addonAfter="Rate"
                               defaultValue={100}
                             />
-                            <InputNumber addonAfter="Quantity" defaultValue={100} />
+                            <InputNumber
+                              addonAfter="Quantity"
+                              defaultValue={100}
+                            />
                             <InputNumber
                               addonBefore="Total"
                               defaultValue={100}
@@ -109,7 +144,10 @@ const Index = () => {
                               addonAfter="Rate"
                               defaultValue={100}
                             />
-                            <InputNumber addonAfter="Quantity" defaultValue={100} />
+                            <InputNumber
+                              addonAfter="Quantity"
+                              defaultValue={100}
+                            />
                             <InputNumber
                               addonBefore="Total"
                               defaultValue={100}
@@ -118,14 +156,19 @@ const Index = () => {
                           </div>
                         </div>
                         <div className="rateWrapper">
-                          <p className="heading">Other Rate(not base on an hourly rate)</p>
-                          <div className="input-fields" >
+                          <p className="heading">
+                            Other Rate(not base on an hourly rate)
+                          </p>
+                          <div className="input-fields">
                             <InputNumber
                               addonBefore="$"
                               addonAfter="Rate"
                               defaultValue={100}
                             />
-                            <InputNumber addonAfter="Quantity" defaultValue={100} />
+                            <InputNumber
+                              addonAfter="Quantity"
+                              defaultValue={100}
+                            />
                             <InputNumber
                               addonBefore="Total"
                               defaultValue={100}
@@ -149,7 +192,6 @@ const Index = () => {
                         </div>
                       </div>
                     </div>
-
                   </>
                 ) : (
                   <>
@@ -160,14 +202,19 @@ const Index = () => {
                       <>
                         <div className="tabWrapper">
                           <div className="rateWrapper">
-                            <p className="heading">Other Rate(not base on an hourly rate)</p>
+                            <p className="heading">
+                              Other Rate(not base on an hourly rate)
+                            </p>
                             <div className="input-fields">
                               <InputNumber
                                 addonBefore="$"
                                 addonAfter="Rate"
                                 defaultValue={100}
                               />
-                              <InputNumber addonAfter="Quantity" defaultValue={100} />
+                              <InputNumber
+                                addonAfter="Quantity"
+                                defaultValue={100}
+                              />
                               <InputNumber
                                 addonBefore="Total"
                                 defaultValue={100}
@@ -177,7 +224,11 @@ const Index = () => {
                           </div>
                           <div className="">
                             <p className="heading">Units of Measure</p>
-                            <Radio.Group defaultValue="a" buttonStyle="solid" className="flex-wrap">
+                            <Radio.Group
+                              defaultValue="a"
+                              buttonStyle="solid"
+                              className="flex-wrap"
+                            >
                               <Radio.Button value="a">Day</Radio.Button>
                               <Radio.Button value="b">Each</Radio.Button>
                               <Radio.Button value="c">Pair</Radio.Button>
@@ -202,19 +253,11 @@ const Index = () => {
                         </div>
                       </>
                     </div>
-
                   </>
-                )
-
-                }
+                )}
               </div>
-
-
-
             </div>
           </div>
-
-
         </div>
       </Style>
     </Sidebar>
--- src/Services/config.js
+++ src/Services/config.js
@@ -8,3 +8,4 @@ export const SAVE_CONTACT = "contact/save";
 export const CONTACT_GET_BY_ID = "contact/getById";
 export const CONTACT_UPDATE = "contact/update";
 export const CONTACT_DELETE = "contact/delate";
+export const LINE_ITEMS_GET = 'lineItem/get'
